48454a33909111ef3160bf7aea73cefce1d9e8e6
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-11-12 06:09:24 -0800

    first commit

---
+++ Reddit-Clone/Reddit-Clone/Reddit_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reddit_CloneApp.swift
+//  Reddit-Clone
+//
+//  Created by suthichai on 12/11/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Reddit_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
