34052966c00c3b6b0ed10883c7d54090bac8182d
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-11-17 07:45:10 -0800

    add UploadGroupPost func

--- Reddit-Clone/Reddit-Clone/ViewModel/UploadPost/UploadPostViewModel.swift
+++ Reddit-Clone/Reddit-Clone/ViewModel/UploadPost/UploadPostViewModel.swift
@@ -9,8 +9,23 @@ import Firebase
 import FirebaseFirestoreSwift
 
 class UploadPostViewModel  {
-        
-    func uploadPost(caption : String) {
+    
+    let postType : PostType
+    
+    init(postType : PostType) {
+        self.postType = postType
+    }
+    
+    func uploadPost() {
+        switch postType {
+        case .user(let caption):
+            uploadUserPost(caption: caption)
+        case .group(let groupID , let groupName ,let caption) :
+            uploadGroupPost(groupID: groupID ,groupName: groupName ,caption: caption)
+        }
+    }
+    
+    func uploadUserPost(caption: String) {
         guard let user = AuthViewModel.shared.currentUser else { return }
         guard let userID = user.id else { return }
         let data = [
@@ -29,4 +44,29 @@ class UploadPostViewModel  {
         }
     }
     
+    func uploadGroupPost(groupID: String , groupName: String ,caption: String) {
+        guard let user = AuthViewModel.shared.currentUser else { return }
+        guard let userID = user.id else { return }
+        let data = [
+            "caption" : caption,
+            "timestamp" : Timestamp(date: Date()),
+            "ownerID" : userID,
+            "ownerUsername" : user.username,
+            "groupID" : groupID,
+            "groupName" : groupName,
+            "likes" : 0
+        ] as [String : Any]
+        
+        Firestore.firestore().collection("posts").addDocument(data: data) { error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+        }
+    }
+}
+
+enum PostType {
+    case user(String)
+    case group(String , String , String)
 }
