64d0cca31f16d3fa831138f00c710aec668ec879
Suthichai Chukaew <suthichai.chu@dome.tu.ac.th>
2021-11-21 00:34:23 -0800

    add CreatePostTextView

---
+++ Reddit-Clone/Reddit-Clone/View/Post/CreatePostTextView.swift
@@ -0,0 +1,64 @@
+//
+//  CreatePostTextView.swift
+//  Reddit-Clone
+//
+//  Created by Kunatip Satsomnuk on 20/11/2564 BE.
+//
+
+import SwiftUI
+
+struct CreatePostTextView: View {
+    @ObservedObject var viewModel : UploadPostViewModel
+    @State var text = ""
+    @State var selection: String?
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                LazyVStack {
+                    Image("reddit-top")
+                }
+                .padding(.bottom, 24)
+                
+                VStack {
+                    Text("Community Name")
+                        .font(.system(size: 24, weight: .semibold))
+                        .padding(.bottom, 10)
+                }
+                VStack(spacing: -16) {
+                    TextArea("Enter your Text...", text: $text)
+                        .padding()
+                }
+                .navigationBarHidden(true)
+                
+                Spacer()
+                
+                Button {
+                    viewModel.uploadUserPost(image: nil, caption: text)
+                } label: {
+                    Text("Post")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 50)
+                        .background(text.isEmpty ? LinearGradient(gradient: Gradient(colors: [Color.gray, Color.gray]), startPoint: .leading, endPoint: .trailing) : LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
+                        .clipShape(Capsule())
+                        .padding()
+                }
+                .disabled(text.isEmpty)
+                
+                NavigationLink(
+                    destination: PostView().navigationBarHidden(true),
+                    label: {
+                        Text("Go Back")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(width: 360, height: 50)
+                            .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.orange]), startPoint: .leading, endPoint: .trailing))
+                            .clipShape(Capsule())
+                            .padding()
+                    }
+                )
+            }
+        }
+    }
+}
